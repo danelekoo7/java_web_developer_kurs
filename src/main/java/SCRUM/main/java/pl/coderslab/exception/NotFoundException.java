ede5f48ec5d98174f9c40f61ca2dc73b15cab86b
danelekoo7 <danieljedrus@gmail.com>
2020-03-05 17:56:45 +0100

    Podczas zajęć z SCRUMa razem z 3 innymi osobami pracowałem nad stworzeniem strony internetowej.

---
+++ src/main/java/SCRUM/main/java/pl/coderslab/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package SCRUM.main.java.pl.coderslab.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/SCRUM/main/java/pl/coderslab/web/LandingPage.java
@@ -0,0 +1,16 @@
+package SCRUM.main.java.pl.coderslab.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("")
+public class LandingPage extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
+    }
+}
